6e07b34c43597f9d74e1d3d2d7476addba4b12bc
malvi <mrp92@njit.edu>
2018-11-01 20:22:34 -0400

    Task:Created a unit test to test inserting a car

---
+++ tests/Unit/InsertCarTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class InsertCarTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testInsertCar()
+    {
+        $car = factory(\App\Car::class)->make();
+        $this->assertDatabaseHas('cars', ["make" => "Ford"]);
+    }
+}
